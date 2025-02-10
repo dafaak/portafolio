ce4417940248b07587acb2b785e78e4bf8ff1fa3
Israel <israel.pe@outlook.es>
2025-02-10 16:38:22 -0500

    feat: home section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 import { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
-
+import { Home } from './components/sections/Home';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -21,6 +21,7 @@ function App() {
       <div className={`max-h-screen max-w-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} className="w-full" />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home/>
       </div>
     </>
   )
