66fd1f9637e4f81ffa27e15a2469c37ed9403d21
Israel <israel.pe@outlook.es>
2025-02-10 10:42:05 -0500

    feat: mobile menu options, styles

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -7,8 +7,31 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                 ? "h-screen opacity-100 pointer-events-auto"
                 : "h-0 opacity-0 pointer-events-none"}
         `}>
-            <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer">
+            <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+                aria-label="Close menu"
+            >
                 &times;
             </button>
+
+            <a href="#home"
+                onClick={() => setMenuOpen(false)}
+                className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+         ${menuOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
+            >Home</a>
+
+            <a href="#about"
+                onClick={() => setMenuOpen(false)}
+                className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                    ${menuOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>About</a>
+
+            <a href="#projects"
+                onClick={() => setMenuOpen(false)}
+                className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                    ${menuOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>Projects</a>
+
+            <a href="#contact"
+                onClick={() => setMenuOpen(false)}
+                className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+    ${menuOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}>Contact</a>
         </div>)
 }
