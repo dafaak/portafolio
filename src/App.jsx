5a2ce5e1e5b2f85fb233cc41d5628fbfb792e7c7
Israel <israel.pe@outlook.es>
2025-02-10 10:29:02 -0500

    feat: added loading screen to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.css';
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css"
 import { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
+import { RocketToMoonLoadingScreen } from './components/BlackHoleLoadingScreen';
+
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleComplete = () => {
+    setLoading(false);
+  };
 
   return (
     <>
+    
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`max-h-screen max-w-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} className="w-full" />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
     </>
   )
 }
