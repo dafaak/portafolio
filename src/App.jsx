dc145ea47822294d1a355dc74ebe5db3c0a860d8
Israel <israel.pe@outlook.es>
2025-12-16 18:56:13 -0500

    refactor: remove  not used function

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
 import './App.css';
-import { LoadingScreen } from './components/LoadingScreen';
+import {LoadingScreen} from './components/LoadingScreen';
 import "./index.css"
-import { useState } from 'react';
-import { Navbar } from './components/Navbar';
-import { MobileMenu } from './components/MobileMenu';
+import {useState} from 'react';
+import {Navbar} from './components/Navbar';
+import {MobileMenu} from './components/MobileMenu';
 import {Home} from './components/sections/Home';
 import {About} from './components/sections/About';
 import {Projects} from './components/sections/Projects';
 import {Contact} from './components/sections/Contact';
 
+
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleComplete = () => {
-    setLoading(false);
-  };
+    return (
+        <>
 
-  return (
-    <>
-    
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div className={`max-h-screen max-w-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} className="w-full" />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Home/>
-        <About/>
-        <Projects/>
-        <Contact/>
-      </div>
-    
-    </>
-  )
+            {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+            <div
+                className={`max-h-screen max-w-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+                <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} className="w-full"/>
+                <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+                <Home/>
+                <About/>
+                <Projects/>
+                <Contact/>
+            </div>
+
+        </>
+    )
 }
 
 export default App
