5fe3dbecd07adbc425786315861ee8a508953c8e
Israel <israel.pe@outlook.es>
2025-12-17 16:55:59 -0500

    feat: load repos from github

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,80 +1,74 @@
-import { RevealOnScroll } from "../RevealOnScroll"
+import ProjectsSectionSkeleton from './ProjectSectionSkeleton.jsx';
+import {RevealOnScroll} from "../RevealOnScroll"
+import {gql} from '@apollo/client';
+import {useQuery} from "@apollo/client/react";
+
+const GET_PINNED_ITEMS = gql`query {
+  viewer {
+    pinnedItems(first: 6, types: [REPOSITORY]) {
+      nodes {
+        ... on Repository {
+          name
+          description
+          url
+                  
+          repositoryTopics(first: 10) { 
+            nodes {
+              topic {
+                name
+              }
+            }
+          }
+                    
+        }
+      }
+    }
+  }
+}`;
+
 export const Projects = () => {
+
+    const {data, error, loading} = useQuery(GET_PINNED_ITEMS);
+
+    if (loading) return <ProjectsSectionSkeleton/>;
+    if (error) return <div>Error: {error.message}</div>;
+
     return (
         <section id="projects" className=" flex items-center justify-center py-20 ">
             <RevealOnScroll>
-            <div className="max-w-5xl mx-auto px-4">
-                <h2 className="text-3xl font-bold mb-8
+                <div className="max-w-5xl mx-auto px-4">
+                    <h2 className="text-3xl font-bold mb-8
             bg-gradient-to-r from-cyan-300 to-purple-600
             bg-clip-text text-transparent text-center">Proyectos</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                        {data.viewer.pinnedItems.nodes.map((repo, index) => (
+
+
+                            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">Typing Wars</h3>
-                        <p className="text-gray-400 mb-4">Multiplayer typing game</p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "NodeJS","Websockets","Express"].map((skill, index) => (<span key={index}
-                                className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20
+                                <h3 className="text-xl font-bold mb-2">{repo.name}</h3>
+                                <p className="text-gray-400 mb-4">{repo.description}</p>
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {repo.repositoryTopics.nodes.map((topic, index) => (
+                                        <span key={index}
+                                              className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20
                             hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition-all">
-                                {skill}
+                                {topic.topic.name}
                             </span>))}
-                        </div>
-                        <div className="flex justify-between mt-4 items-center">
-                            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors">View Project ➡️</a>
-                        </div>
+                                </div>
+                                <div className="flex justify-between mt-4 items-center">
+                                    <a href={repo.url} target='_blank'
+                                       className="text-blue-400 hover:text-blue-300 transition-colors">View
+                                        Project
+                                        ➡️</a>
+                                </div>
+                            </div>
+
+                        ))}
+
                     </div>
-
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">Typing Wars</h3>
-                        <p className="text-gray-400 mb-4">Multiplayer typing game</p>
-                        <div>
-                            {["React", "NodeJS","Websockets","Express"].map((skill, index) => (<span key={index}
-                                className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition-all">
-                                {skill}
-                            </span>))}
-                        </div>
-                        <div className="flex justify-between mt-4 items-center">
-                            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors">View Project ➡️</a>
-                        </div>
-                    </div>
-
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">Typing Wars</h3>
-                        <p className="text-gray-400 mb-4">Multiplayer typing game</p>
-                        <div>
-                            {["React", "NodeJS","Websockets","Express"].map((skill, index) => (<span key={index}
-                                className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition-all">
-                                {skill}
-                            </span>))}
-                        </div>
-                        <div className="flex justify-between mt-4 items-center">
-                            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors">View Project ➡️</a>
-                        </div>
-                    </div>
-
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">Typing Wars</h3>
-                        <p className="text-gray-400 mb-4">Multiplayer typing game</p>
-                        <div>
-                            {["React", "NodeJS","Websockets","Express"].map((skill, index) => (<span key={index}
-                                className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition-all">
-                                {skill}
-                            </span>))}
-                        </div>
-                        <div className="flex justify-between mt-4 items-center">
-                            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors">View Project ➡️</a>
-                        </div>
-                    </div>
-
-
                 </div>
-            </div>
             </RevealOnScroll>
         </section>
     )
